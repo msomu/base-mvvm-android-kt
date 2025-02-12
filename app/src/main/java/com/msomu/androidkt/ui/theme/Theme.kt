38e00ab14225631c8beef1567d8b8b9dd4d138b4
Somasundaram M <msomasundaram93@gmail.com>
2025-02-12 13:29:02 +0530

    refactor : removed unused imports

--- app/src/main/java/com/msomu/androidkt/ui/theme/Theme.kt
+++ app/src/main/java/com/msomu/androidkt/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.msomu.androidkt.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
